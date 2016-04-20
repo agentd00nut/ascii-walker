2284464b77fe898e374e229d532348d0ea0b0b60
abrothers <abrothers@truoptik.com>
2016-04-20 16:47:24 -0400

    Use addch for speed increase

--- ascii-walker.c
+++ ascii-walker.c
@@ -10,13 +10,16 @@
 
 static int n_cols;
 static int n_rows;
-
+static int last_color;
 
 void sig_handler(int signo){
     if (signo == SIGINT){
         char* white     = "\e[97m";
         printf("%sCleaning up ...\n", white);
+        
+        scr_dump("my_file");
         endwin();
+        
         exit(1);
     }
 }
@@ -26,71 +29,38 @@ int print_map(char **grid, int x, int y){
     int i;
     int j;
 
-    init_pair(1, COLOR_BLACK, COLOR_BLACK);
-    init_pair(2, COLOR_WHITE, COLOR_BLACK);
-    init_pair(3, COLOR_GREEN, COLOR_BLACK);
-    init_pair(4, COLOR_MAGENTA, COLOR_BLACK);
-    init_pair(5, COLOR_CYAN, COLOR_BLACK);
-    init_pair(6, COLOR_YELLOW, COLOR_BLACK);
 
-
-    char *buf = (char*)malloc(sizeof(char)*(n_cols*n_rows*13));
-    //char temp_char = '';
     char current_char = ' ';
     int  current_color = 1;
-
-    char poop[2];
-
 
 
     for(i = 0; i < n_rows; ++i){
 
         for(j = 0; j < n_cols; ++j){
 
-            /*if( i == y && j == x)
-            {
-                temp_char = grid[i][j];
-                grid[i][j] = '@';
-            }*/
-
-            /*if(grid[i][j] == '@'){
-                //iter += sprintf(buf+iter, "%c", grid[i][j] );
-                mvprintw(i,j, "@")
-            }else if(grid[i][j] == '.'){
-                //iter += sprintf(buf+iter, "%c", grid[i][j] );
-            }else if(grid[i][j] == '*'){
-                iter += sprintf(buf+iter, "%c", grid[i][j] );
-            }else if(grid[i][j] == '%'){
-                iter += sprintf(buf+iter, "%c", grid[i][j] );
-            }else if(grid[i][j] == '&'){
-                iter += sprintf(buf+iter, "%c", grid[i][j] );
-            }else{
-                iter += sprintf(buf+iter, "%c", grid[i][j]);
-            }*/
-            poop[0] = grid[i][j];
-            poop[1] = '\0';
-
             current_char = grid[i][j];
 
-            if(current_char == '@'){ current_color = 2; }
-            if(current_char == '.'){ current_color = 3; }
-            if(current_char == ','){ current_color = 4; }
-            if(current_char == '*'){ current_color = 5; }
-            if(current_char == '#'){ current_color = 6; }
+            if(current_char == '.'){ 
+                current_color = 3; 
+            }
+            else if(current_char == ','){ 
+                current_color = 4; 
+            }
+            else if(current_char == '*'){ 
+                current_color = 5; 
+            }
+            else if(current_char == '#'){ 
+                current_color = 6; 
+            }
 
-            attron(COLOR_PAIR(current_color));
-            mvprintw(i,j, poop);
-            attroff(COLOR_PAIR(current_color));
+            move(i,j);
+            addch(current_char | COLOR_PAIR(current_color)  );
 
         }
-
-//        iter += sprintf(buf+iter, "\n");
     }
     
-    attron(COLOR_PAIR(2));
-    mvprintw(y,x,"@");
-    attroff(COLOR_PAIR(2));
-    //redrawwin(stdscr);
+    move(y,x);
+    addch('@' | COLOR_PAIR(2) );
     refresh();
 
     return 1;
@@ -102,13 +72,14 @@ int main(int argc, char **argv){
     int i;  //the all around iterating bitch
     int j;  //the bottom bitch
     int k;
-    int chances[8];
+
     char tile = ' ';
     char player = '@';
     char path = '.';
     char second_visit = ',';
     char third_visit = '*';
     char fourth_visit = '#';
+
     srand(time(NULL));              //seed rng
     struct winsize w;               //get tty size
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
@@ -120,13 +91,21 @@ int main(int argc, char **argv){
     int vertical;
     int direction;
     int total;
-    int temp;
     int sleep_time = 40000;
 
     initscr();          /* Start curses mode          */
     noecho(); // Don't echo any keypresses
     curs_set(FALSE); // Don't display a cursor
     start_color();
+
+
+    init_pair(1, COLOR_BLACK, COLOR_BLACK);
+    init_pair(2, COLOR_WHITE, COLOR_BLACK);
+    init_pair(3, COLOR_GREEN, COLOR_BLACK);
+    init_pair(4, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(5, COLOR_CYAN, COLOR_BLACK);
+    init_pair(6, COLOR_YELLOW, COLOR_BLACK);
+
     // LAZY ARGS
     if(argc > 1)
     {
@@ -146,9 +125,6 @@ int main(int argc, char **argv){
         //pass
     }
     
-
-
-
 
     while(print_map(grid, x, y)){
         total = 96;
@@ -189,9 +165,9 @@ int main(int argc, char **argv){
         }
         //wrap around
         if(horizontal >= n_cols){ horizontal = 0; }
-        if(horizontal < 0){ horizontal = n_cols-1; }
+        else if(horizontal < 0){ horizontal = n_cols-1; }
         if(vertical >= n_rows ){ vertical = 0; }
-        if(vertical < 0){ vertical = n_rows-1; }
+        else if(vertical < 0){ vertical = n_rows-1; }
 
         if( grid[y][x] == path ){ grid[y][x] = second_visit; } 
         else if(   grid[y][x] == second_visit){ grid[y][x] = third_visit; }
@@ -202,11 +178,9 @@ int main(int argc, char **argv){
         
         x = horizontal;
         y = vertical;
-        //usleep(65000); NICE
-        //usleep(50000);
+
         usleep(sleep_time);
-        //usleep(100000000);
-        //sleep(1);
+
     }
     return 0;
 }
